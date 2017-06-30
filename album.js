567d262bf2bcf31a91aede33baaa6999dea69aba
Thamer26 <thamer.alhejailan1@gmail.com>
2017-06-30 16:32:14 +0400

    Add files via upload

--- album.js
+++ album.js
@@ -50,50 +50,53 @@ var createSongRow = function(songNumber, songName, songLength) {
      + '</tr>'
      ;
 
-    return template;
-};
+     var $row = $(template);
+     var clickHandler = function() {
+     // clickHandler logic
+     };
+
+
+     var onHover = function(event) {
+         // Placeholder for function logic
+     };
+     var offHover = function(event) {
+         // Placeholder for function logic
+     };
+     // #1
+       $row.find('.song-item-number').click(clickHandler);
+       // #2
+       $row.hover(onHover, offHover);
+       // #3
+       return $row;};
 
 var setCurrentAlbum = function(album) {
     // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    var $albumTitle = $('.album-view-title');
+   var $albumArtist = $('.album-view-artist');
+   var $albumReleaseInfo = $('.album-view-release-info');
+   var $albumImage = $('.album-cover-art');
+   var $albumSongList = $('.album-view-song-list');
+
 
     // #2
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+    $albumTitle.text(album.title);
+ $albumArtist.text(album.artist);
+ $albumReleaseInfo.text(album.year + ' ' + album.label);
+ $albumImage.attr('src', album.albumArtUrl);
 
     // #3
-    albumSongList.innerHTML = '';
+    $albumSongList.empty();
 
     // #4
     for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      $albumSongList.append($newRow);
+
     }
 };
 
-var clickHandler = function(targetElement) {
 
-var songItem = getSongItem(targetElement);
-  if (currentlyPlayingSong === null) {
-  songItem.innerHTML = pauseButtonTemplate;
-  currentlyPlayingSong = songItem.getAttribute('data-song-number');
-} else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-     songItem.innerHTML = playButtonTemplate;
-     currentlyPlayingSong = null;
-   } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-       var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-       currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-       songItem.innerHTML = pauseButtonTemplate;
-       currentlyPlayingSong = songItem.getAttribute('data-song-number');
-   }
-};
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
+
 
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -103,26 +106,11 @@ var currentlyPlayingSong = null;
 
 
 
-window.onload = function() {
-  songListContainer.addEventListener('mouseover', function(event) {
-    for (var i = 0; i < songRows.length; i++) {
-     songRows[i].addEventListener('mouseleave', function(event) {
-       // #1
-           var songItem = getSongItem(event.target);
-           var songItemNumber = songItem.getAttribute('data-song-number');
+$(document).ready(function() {
 
-           // #2
-           if (songItemNumber !== currentlyPlayingSong) {
-               songItem.innerHTML = songItemNumber;
-           }
-     });
-
-
-     songRows[i].addEventListener('click', function(event) {
-       clickHandler(event.target);
 
             });
-              }
+
     // Only target individual song rows during event delegation
     if (event.target.parentElement.className === 'album-view-song-item') {
       event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
